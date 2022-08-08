33ec112259eacb9a2d35fea1179c21f7d0678982
omarajraoui <omarajraoui5@gmail.com>
2022-08-09 01:27:36 +0200

    tlrh security

---
+++ src/main/java/com/tlrh/security/UserServiceApplication.java
@@ -0,0 +1,49 @@
+package com.tlrh.security;
+
+import com.tlrh.security.domain.Role;
+import com.tlrh.security.domain.User;
+import com.tlrh.security.service.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "JohnTravolta", "john", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "WillSmith", "will", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "JimCarry", "jim", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "ArnoldSchwarzenegger", "arnold", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("john", "ROLE_USER");
+			userService.addRoleToUser("will", "ROLE_MANAGER");
+			userService.addRoleToUser("jim", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_USER");
+		};
+	}
+
+}
---
+++ src/main/java/com/tlrh/security/service/UserService.java
@@ -0,0 +1,19 @@
+package com.tlrh.security.service;
+
+import com.tlrh.security.domain.Role;
+import com.tlrh.security.domain.User;
+
+import java.util.List;
+
+/**
+ * @author Get Arrays (https://www.getarrays.io/)
+ * @version 1.0
+ * @since 7/10/2021
+ */
+public interface UserService {
+    User saveUser(User user);
+    Role saveRole(Role role);
+    void addRoleToUser(String username, String roleName);
+    User getUser(String username);
+    List<User>getUsers();
+}
